e356d6d14d4c8454c9cbb58ce891a1fe837b2f4e
kumarmudur <kumarmudur10@gmail.com>
2026-05-30 07:41:50 -0400

    todo card toggle and delte functionality changes

--- app/components/TodoCard.tsx
+++ app/components/TodoCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from "react";
+//import { deleteTodo } from "@/lib/api";
 import { Todo } from "@/types/todo";
 
 interface TodoCardProps {
@@ -8,9 +9,15 @@ interface TodoCardProps {
 
 const TodoCard = ({ todo }: TodoCardProps) => {
     const { title } = todo;
-    const [completed, setCompleted] = useState(
-        todo.completed
-    );
+    const [completed, setCompleted] = useState(todo.completed);
+
+    const handleToggle = async () => {
+        setCompleted(!completed);
+    }
+
+    const handleDelete = async () => {
+       // await deleteTodo(todo.id);
+    }
 
     return (
         <div className="border rounded-lg p-5 shadow-sm bg-white">
@@ -18,6 +25,19 @@ const TodoCard = ({ todo }: TodoCardProps) => {
                 <h2 className={`text-lg font-semibold ${completed ? 'line-through text-black' : ""}`}>
                     { title }
                 </h2>
+
+                <div className="flex gap-3">
+                    <button
+                        onClick={handleToggle}
+                        className="border px-4 py-2 rounded"
+                    >
+                        { completed ? 'Undo': 'Complete'}
+                    </button>
+
+                    <button onClick={handleDelete} className="bg-red-500 text-white px-4 py-2 rounded">
+                        Delete
+                    </button>
+                </div>
             </div>
         </div>
     )
