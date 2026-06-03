67f73084360e5608d63d7cda952428b0d04ee7eb
kumarmudur <kumarmudur10@gmail.com>
2026-06-03 06:34:23 -0400

    TechCard component

--- app/components/TodoCard.tsx
+++ app/components/TodoCard.tsx
@@ -1,46 +1,48 @@
-'use client';
+"use client";
 import { useState } from "react";
-//import { deleteTodo } from "@/lib/api";
+import { deleteTodo } from "@/lib/api";
 import { Todo } from "@/types/todo";
 
 interface TodoCardProps {
-    todo: Todo
+  todo: Todo;
 }
 
 const TodoCard = ({ todo }: TodoCardProps) => {
-    const { title } = todo;
-    const [completed, setCompleted] = useState(todo.completed);
+  const { title } = todo;
+  const [completed, setCompleted] = useState(todo.completed);
 
-    const handleToggle = async () => {
-        setCompleted(!completed);
-    }
+  const handleToggle = async () => {
+    setCompleted(!completed);
+  };
 
-    const handleDelete = async () => {
-       // await deleteTodo(todo.id);
-    }
+  const handleDelete = async () => {
+    await deleteTodo(todo.id);
+  };
 
-    return (
-        <div className="border rounded-lg p-5 shadow-sm bg-white">
-            <div className="flex items-center justify-between">
-                <h2 className={`text-lg font-semibold ${completed ? 'line-through text-black' : ""}`}>
-                    { title }
-                </h2>
+  return (
+    <div className="border rounded-lg p-5 shadow-sm bg-white">
+      <div className="flex items-center justify-between">
+        <h2
+          className={`text-lg font-semibold ${completed ? "line-through text-black" : ""}`}
+        >
+          {title}
+        </h2>
 
-                <div className="flex gap-3">
-                    <button
-                        onClick={handleToggle}
-                        className="border px-4 py-2 rounded"
-                    >
-                        { completed ? 'Undo': 'Complete'}
-                    </button>
+        <div className="flex gap-3">
+          <button onClick={handleToggle} className="border px-4 py-2 rounded">
+            {completed ? "Undo" : "Complete"}
+          </button>
 
-                    <button onClick={handleDelete} className="bg-red-500 text-white px-4 py-2 rounded">
-                        Delete
-                    </button>
-                </div>
-            </div>
+          <button
+            onClick={handleDelete}
+            className="bg-red-500 text-white px-4 py-2 rounded"
+          >
+            Delete
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default TodoCard;
